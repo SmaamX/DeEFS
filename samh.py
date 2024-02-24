d45d0ba2ed881d5722f812cc3d1207623a8976cc
SmaamX <90418723+SmaamX@users.noreply.github.com>
2024-02-24 14:25:08 +0330

    Update samh.py

--- samh.py
+++ samh.py
@@ -13,7 +13,7 @@ if user_input == "1":
     for i in range(6):v.append(randc())
     else:return ":".join(v)
   ma=randma();print("\n\u001b[0m\u001b[37mwlan random mac",ma);sy("svc wifi disable");sy("su -c echo "+ma+" > /efs/wifi/.mac.cob && echo "+ma+" > /efs/wifi/.mac.info");sy("svc wifi enable")
-  mab=randma();print("\n\u001b[0m\u001b[37mbt random mac",mab);sy("svc bluetooth disable");sy("su -c echo "+mab+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable)
+  mab=randma();print("\n\u001b[0m\u001b[37mbt random mac",mab);sy("svc bluetooth disable");sy("su -c echo "+mab+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable")
 elif user_input == "2":user_inputb=input("\n\u001b[37mBT set -> ");["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",":"] in user_inputb == False or len(user_inputb) != 17 if exit(1) else sy("svc bluetooth disable");sy("su -c echo "+mab+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable")
 elif user_input == "3":user_inputb=input("\n\u001b[37mWLAN set -> ");["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",":"] in user_inputb == False or len(user_inputw) != 17 if exit(1) else sy("svc wifi disable");sy("su -c echo "+user_inputw+" > /efs/wifi/.mac.cob && echo "+user_inputw+" > /efs/wifi/.mac.info");sy("svc wifi enable")
 elif user_input == "0":exit(0)
