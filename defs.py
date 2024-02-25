3288308e4bcee6c5b7eb53f1a8e03fd7e72c2d1d
SmaamX <90418723+SmaamX@users.noreply.github.com>
2024-02-25 20:28:18 +0330

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,8 +1,9 @@
-#DeEFS 1.9
+#DeEFS 1.6
 from random import choice as rch
 from os import system as sy
 from time import sleep as sl
 import subprocess,re
+def wcon(bssid,pasw,typed):sy("su -c cmd wifi connect-network "+bssid+" "+typed+" "+(pasw if typed != "open" else ""))
 def bmac(val):
   if val == 0:mc = open("/efs/wifi/.mac.cob");cob=mc.read();return cob
   elif val == 1:mc = open("/efs/bluetooth/bt_addr");cob=mc.read();return cob
@@ -23,10 +24,10 @@ def dwlan(mac,rf=False):
   if mac == "rand":
     ma=randma();
     if rf == False:sy("su -c svc wifi disable")
-    elif rf == True:sy("su -c svc wifi disable");sy("su -c service call wifi 47")
+    if rf == True:sy("su -c svc wifi disable");sy("service call wifi 47")
     sy("su -c echo "+ma+" > /efs/wifi/.mac.cob && echo "+ma+" > /efs/wifi/.mac.info")
     if rf == False:sy("su -c svc wifi enable")
-    elif rf == True:sy("su -c service call wifi 49")
+    if rf == True:sy("service call wifi 49");sy("su -c svc wifi enable")
     return ma
   else:
     buffmc(mac) == False if None else sy("su -c svc wifi disable");sy("su -c echo "+mac+" > /efs/wifi/.mac.cob && echo "+mac+" > /efs/wifi/.mac.info");sy("su -c svc wifi enable");return(0)
@@ -43,12 +44,12 @@ def adbt(reg,out=False):
   back=bmac(1)
   for i in range(reg):mac1=dbt("rand");sy("su -c dumpsys bluetooth_manager >"+(("> /dev/null") if out == False else ""));print("\u001b[32m["+str(i+1)+","+mac1+"] bt packet send")
   dbt(str(back))
-def wjammer(reg,s,tar):
+def wjammer(reg,s,tar,typed="open",pasw=""):
   back=bmac(0)
   for i in range(reg):
     if tar == "rand":macs=dumpwlan();macx=rch(macs)
     else: macx=tar
-    sl(s);dwlan(str(macx),rf=True);print("\u001b[32m["+str(i+1)+","+macx+"] wlan jammer packet send")
+    sl(s);dwlan(str(macx),rf=True);wcon(macx,pasw,typed);print("\u001b[32m["+str(i+1)+","+macx+"] wlan jammer packet send")
   dwlan(str(back))
 def cusefs(dict,value):sy("su -c echo "+value+" > /efs/"+dict)
 #jump_out
