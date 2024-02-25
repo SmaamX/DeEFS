7e21d97583a8ccce19ce2eb2f42c05325e952908
SmaamX <90418723+SmaamX@users.noreply.github.com>
2024-02-25 13:49:32 +0330

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,6 +1,9 @@
-#DeEFS 1.2
+#DeEFS 1.4
 from random import choice as rch
 from os import system as sy
+def buffmc(mac):
+  if any(cha in ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",":"] for cha in mac) or len(mac) != 17:return True
+  else:return False
 def randma():
   def randc():
     v=[]
@@ -13,11 +16,11 @@ def dwlan(mac):
   if mac == "rand":
     ma=randma();sy("svc wifi disable");sy("su -c echo "+ma+" > /efs/wifi/.mac.cob && echo "+ma+" > /efs/wifi/.mac.info");sy("svc wifi enable");return ma
   else:
-    ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",":"] in mac == False or len(mac) != 17 if exit(1) else sy("svc wifi disable");sy("su -c echo "+mac+" > /efs/wifi/.mac.cob && echo "+mac+" > /efs/wifi/.mac.info");sy("svc wifi enable");return(0)
+    buffmc(mac) == False if None else sy("svc wifi disable");sy("su -c echo "+mac+" > /efs/wifi/.mac.cob && echo "+mac+" > /efs/wifi/.mac.info");sy("svc wifi enable");return(0)
 def dbt(mac):
   if mac == "rand":
     mab=randma();sy("svc bluetooth disable");sy("su -c echo "+mab+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable");return mab
   else:
-    ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",":"] in mac == False or len(mac) != 17 if exit(1) else sy("svc bluetooth disable");sy("su -c echo "+mac+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable");return(0)
+    buffmc(mac) == False if None else sy("svc bluetooth disable");sy("su -c echo "+mac+" > /efs/bluetooth/bt_addr");sy("svc bluetooth enable");return(0)
 def adwlan(reg):
   for i in range(reg):mac1=dwlan("rand");sy("dumpsys wifi scan > /dev/null");print("\u001b[32m["+str(i)+","+mac1+"] packet send")
